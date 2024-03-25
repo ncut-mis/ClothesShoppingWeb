7b1d6d06e4672c40b84b7ee0c86b0c485e371622
3B032080 <kasper1116.kh@gmail.com>
2024-03-25 16:45:04 +0800

    新增order跟order_detials的model等

---
+++ app/Http/Controllers/OrderDetialController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\order_detial;
+use App\Http\Requests\Storeorder_detialRequest;
+use App\Http\Requests\Updateorder_detialRequest;
+
+class OrderDetialController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Storeorder_detialRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(order_detial $order_detial)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(order_detial $order_detial)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Updateorder_detialRequest $request, order_detial $order_detial)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(order_detial $order_detial)
+    {
+        //
+    }
+}
