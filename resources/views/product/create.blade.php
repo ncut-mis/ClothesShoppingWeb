75e3d2eb468653740115376d26830c4550142a0d
3B032063 <3B032063@gm.student.ncut.edu.tw>
2024-03-28 05:16:35 +0800

    product create

---
+++ resources/views/product/create.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>商品新增</h1>
+    <form method="post" action="">
+        <div>
+            <label>商品名稱</label>
+            <input type="text" name="name" placeholder="Name" />
+        </div>
+        <div>
+            <label>商品數量</label>
+            <input type="text" name="quantity" placeholder="Quantity" />
+        </div>
+        <div>
+            <label>商品價格</label>
+            <input type="text" name="price" placeholder="Price" />
+        </div>
+        <div>
+            <label>商品描述</label>
+            <input type="text" name="description" placeholder="Description" />
+        </div>
+        <div>
+            <input type="submit" value="新增" />
+        </div>
+    </form>
+</body>
+</html>
