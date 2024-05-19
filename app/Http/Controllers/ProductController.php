ecef0cbd9e178250330eb77a7d15b5520769a03d
3B032080 <kasper1116.kh@gmail.com>
2024-05-19 16:50:19 +0800

    顯示留言修改

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -105,9 +105,12 @@ class ProductController extends Controller
         //image可修改成取亂數隨機顯示商品圖片，也可取出所有圖片，也可在Model去定義圖片顯示方法
         $image = ProductPhoto::Where('product_id', '=', $product->id)->first();
         $combinations = Combination::Where('product_id', '=', $product->id)->paginate(2);
-        $orders = Order::with('details')->whereHas('details', function ($query) use ($product) {
-            $query->where('product_id', $product->id);
-        })->get();
+        $orders = Order::with('details')
+            ->whereHas('details', function ($query) use ($product) {
+                $query->where('product_id', $product->id);
+            })
+            ->where('status', 3)
+            ->get();
 
         if (Auth::check()) {
             // 用戶已登入，使用 auth layout
